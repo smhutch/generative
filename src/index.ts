b88e2b38a1f98d160f097292c7dffca354144829
Scott Hutcheson <scott@smhutch.co.uk>
2020-10-11 11:44:12 +0100

    use instanced mesh

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import * as THREE from "three";
-import Stats from "stats.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { createRandom } from "canvas-sketch-util/random";
 import { lerp } from "canvas-sketch-util/math";
@@ -8,18 +7,16 @@ function getAntialias() {
   return window.devicePixelRatio > 1 ? false : true;
 }
 
-type Item = {
-  shape: THREE.Mesh;
-  noise: number;
-};
-
 const random = createRandom();
 
 const width = window.innerWidth;
 const height = window.innerHeight;
 
 const scene = new THREE.Scene();
+
 const camera = new THREE.PerspectiveCamera(100, width / height, 1, 1000);
+camera.position.set(40, 100, 120);
+
 const renderer = new THREE.WebGLRenderer({
   alpha: false,
   antialias: getAntialias(),
@@ -28,50 +25,20 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(width, height);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxBufferGeometry();
+const geometry = new THREE.IcosahedronBufferGeometry();
 const material = new THREE.MeshLambertMaterial();
-const baseShape = new THREE.Mesh(geometry, material);
+material.reflectivity = 100;
 
-const grid = 15;
-const items: Item[] = [];
-for (let y = 0; y < grid; y++) {
-  const py = y / (grid - 1);
-  for (let x = 0; x < grid; x++) {
-    const px = x / (grid - 1);
-    for (let z = 0; z < grid; z++) {
-      const pz = z / (grid - 1);
-      const noise = random.noise3D(py, px, pz);
+const grid = 12;
+const count = Math.pow(grid, 3);
 
-      const space = 45;
-      const shape = baseShape.clone();
-      shape.position.x = lerp(-space, space, px);
-      shape.position.y = lerp(-space, space, py);
-      shape.position.z = lerp(-space, space, pz);
+const mesh = new THREE.InstancedMesh(geometry, material, count);
+// optimize for updates every frame
+mesh.instanceMatrix.setUsage(THREE.DynamicDrawUsage);
+scene.add(mesh);
 
-      const scale = Math.abs(5 * noise) + 2;
-      shape.scale.x = scale;
-      shape.scale.y = scale;
-      shape.scale.z = scale;
-
-      const rotation = random.range(0, 2);
-      shape.rotation.x = rotation;
-      shape.rotation.y = rotation;
-      shape.rotation.z = rotation;
-
-      // Render shape
-      scene.add(shape);
-
-      // Expose shape to parent scope
-      items.push({
-        shape,
-        noise,
-      });
-    }
-  }
-}
-
+// Allow user to move around central axis
 const controls = new OrbitControls(camera, renderer.domElement);
-camera.position.set(40, 120, 80);
 controls.autoRotate = true;
 controls.enableDamping = true;
 controls.autoRotateSpeed = 0.3;
@@ -80,27 +47,66 @@ controls.minDistance = 50;
 controls.maxDistance = 300;
 controls.update();
 
-camera.position.z = 120;
-
 const topLight = new THREE.SpotLight(0xffffff);
 topLight.intensity = 1;
-topLight.position.y = 100;
-topLight.distance = 200;
+topLight.position.y = grid * 10;
+topLight.distance = grid * 10 * 2;
+topLight.decay = 2;
 scene.add(topLight);
 
-// const duration = 5; // seconds
-// const durationMs = duration * 1000;
-// const startTime = performance.now();
+// Variables required to update meshed instance
+const matrix = new THREE.Matrix4();
+const position = new THREE.Vector3();
+const rotation = new THREE.Euler();
+const quaternion = new THREE.Quaternion();
+const scale = new THREE.Vector3();
+
+// animation loop
+const duration = 5; // seconds
+const durationMs = duration * 1000;
+const startTime = performance.now();
 
 function animate(timestamp: number) {
-  requestAnimationFrame(animate);
-  // const time = timestamp - startTime;
-  // const playhead = time % durationMs;
-  // const t = playhead / durationMs;
-  // const r = Math.sin(t * Math.PI);
+  // time from init to current render timestamp
+  const time = timestamp - startTime;
+  // loop from 0 to {{duration}}
+  const playhead = time % durationMs;
+  // percentage from 0 to 1, where 1 is {{duration}}
+  const t = playhead / durationMs;
+  // 0 to 1 to 0 in {{duration}} seconds
+  const r = Math.sin(t * Math.PI);
 
+  var i = 0;
+  for (let x = 0; x < grid; x++) {
+    const px = x / (grid - 1);
+    for (let y = 0; y < grid; y++) {
+      const py = y / (grid - 1);
+      for (let z = 0; z < grid; z++) {
+        const pz = z / (grid - 1);
+        const noise = random.noise3D(px, py, pz);
+        const posLimit = grid * 5;
+
+        position.x = lerp(-posLimit, posLimit, px);
+        position.y = lerp(-posLimit, posLimit, py);
+        position.z = lerp(-posLimit, posLimit, pz);
+
+        rotation.x = rotation.y = rotation.z = Math.PI;
+
+        quaternion.setFromEuler(rotation);
+
+        scale.x = scale.y = scale.z = 4 * noise + 3;
+
+        matrix.compose(position, quaternion, scale);
+        mesh.setMatrixAt(i, matrix);
+        i++;
+      }
+    }
+  }
+
+  mesh.instanceMatrix.needsUpdate = true;
   controls.update();
   renderer.render(scene, camera);
+  requestAnimationFrame(animate);
 }
 
 requestAnimationFrame(animate);
